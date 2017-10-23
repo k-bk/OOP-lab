beaf71425b0e6803701314d1be04d6b83bc68fbe
karolBak <bak.karolek@gmail.com>
2017-10-23 13:02:19 +0100

    Sth with tests not working.

---
+++ src/main/agh/cs/lab2/ActionParser.java
@@ -0,0 +1,18 @@
+package agh.cs.lab2;
+import java.util.*;
+
+public class ActionParser {
+    public List<MoveDirection> parse(String[] actions) {
+        List<MoveDirection> moves = new ArrayList<>();
+        for(String s : actions) {
+            if (s.equals("forward") || s.equals("f")) {
+                MoveDirection newMove = MoveDirection.Forward;
+            moves.add(newMove);
+            } else if(s.equals("backward") || s.equals("b")) {
+                MoveDirection newMove = MoveDirection.Backward;
+                moves.add(newMove);
+            }
+        }
+        return moves;
+    }
+}
