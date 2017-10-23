c1d1d44f72afc9871e7e3dacc4d6345d68f8800a
karolBak <bak.karolek@gmail.com>
2017-10-23 12:06:48 +0200

    Added test junit4 files

---
+++ src/test/agh/cs/lab2/MapDirectionTest.java
@@ -0,0 +1,20 @@
+package agh.cs.lab2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MapDirectionTest {
+    @Test
+    public void next() throws Exception {
+    }
+
+    @Test
+    public void previous() throws Exception {
+    }
+
+    @Test
+    public void toString() throws Exception {
+    }
+
+}
